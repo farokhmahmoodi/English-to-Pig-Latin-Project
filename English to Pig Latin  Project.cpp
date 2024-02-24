1b5a805a7d0aa969284a20ad6ef1aef7ad051238
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-02-24 16:32:21 -0600

    Add files via upload

--- English to Pig Latin  Project.cpp
+++ English to Pig Latin  Project.cpp
@@ -4,17 +4,24 @@ Your program will then output this Pig Latin to the screen.
 
 Note: While there are many ways to do conversions to pig latin, I will require that you follow the procedures below, all 
 of which will use the following structure:
+struct Word { string english; string piglatin; }; 
 
-struct Word { string english; string piglatin; }; Part 1. Write a function that takes in an English sentence as one string. 
-This function should first calculate how many “words” are in the sentence (words being substrings separated by whitespace). It should then allocate a dynamic array of size equal to the number of words. The array contains Word structures (i.e. array of type Word). The function would then store each word of that sentence to the english field of the corresponding structure. The function should then return this array to the calling function using the return statement, along with the array size using a reference parameter.
+Part 1. Write a function that takes in an English sentence as one string. 
+This function should first calculate how many “words” are in the sentence (words being substrings separated by whitespace). 
+It should then allocate a dynamic array of size equal to the number of words. The array contains Word structures 
+(i.e. array of type Word). The function would then store each word of that sentence to the english field of the 
+corresponding structure. The function should then return this array to the calling function using the return statement, 
+along with the array size using a reference parameter.
 
 This function should also remove all capitalization and special characters other than letters. Implement the function with 
 the following prototype
+Word * splitSentence(const string words, int &size); 
 
-Word * splitSentence(const string words, int &size); Part 2. Write a function that takes in an array of Word structures 
-and the size of the array and converts each english field to the corresponding piglatin field.
+Part 2. Write a function that takes in an array of Word structures and the size of the array and converts each english 
+field to the corresponding piglatin field.
+void convertToPigLatin(Word [] wordArr, int size); 
 
-void convertToPigLatin(Word [] wordArr, int size); To do this conversion, if a word starts with a consonant, the piglatin 
+To do this conversion, if a word starts with a consonant, the piglatin 
 conversion of the word involves moving the first letter of the word to the end of the string and then adding “ay” to the 
 end.
 
